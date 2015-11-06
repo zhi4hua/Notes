1dd8b2b9c1b104a63cc41382722dd2d4553c7c84
zhi4hua <zhi4hua@yeah.net>
2015-11-06 11:05:57 +0800

    Update girls.sql

--- SQL_MySQL/chapter-ten/girls.sql
+++ SQL_MySQL/chapter-ten/girls.sql
@@ -9,3 +9,8 @@ INSERT INTO girls
 	VALUES(	1, "Jane", 3),
 		(2, "Sally", 4),
 		(3, "Cindy", 1);
+		
+# 关键表
+ALTER TABLE girls ADD CONSTRAINT toys_toy_id_fk 
+	FOREIGN KEY(toy_id)
+	REFERENCES toys(toy_id)
