a6a53a0a050cccd8bf6442381908affe687d1d1d
zhi4hua <zhi4hua@yeah.net>
2015-11-06 10:55:14 +0800

    Update girls.sql

--- SQL_MySQL/chapter-ten/girls.sql
+++ SQL_MySQL/chapter-ten/girls.sql
@@ -1,11 +1,11 @@
 # 创建表 girls
-CREATE TABLE girls {
-	gir_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY DEFAULT 0,
+CREATE TABLE girls (
+	gir_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
 	girl VARCHAR(50),
 	toy_id INT
-};
+);
 
 INSERT INTO girls 
-	VALUES(	1, "Jane", 3);
-				(2, "Sally", 4),
-				(3, "Cindy", 1);
+	VALUES(	1, "Jane", 3),
+		(2, "Sally", 4),
+		(3, "Cindy", 1);
