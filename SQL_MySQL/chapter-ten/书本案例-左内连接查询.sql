6b0fe17030bc99addf5065bdeeeaa9a69ef7f35b
zhi4hua <zhi4hua@yeah.net>
2015-11-07 10:34:01 +0800

    Update 书本案例-左内连接查询.sql

--- SQL_MySQL/chapter-ten/书本案例-左内连接查询.sql
+++ SQL_MySQL/chapter-ten/书本案例-左内连接查询.sql
@@ -1,7 +1,8 @@
 # 查询两表中"姓名"与"玩具"的对应
 
+
 SELECT g.girl, t.toy
-	FORM girls AS g
-		LEFT OUTER JOIN
-	toys AS t
-	ON t.toy_id = g.toy_id;
+  FROM girls g
+    LEFT OUTER JOIN 
+toys t
+  ON g.toy_id = t.toy_id
