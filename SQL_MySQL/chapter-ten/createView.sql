dac00174bfdb8c8d45c6e888b5b6059e8d2234b8
zhi4hua <zhi4hua@yeah.net>
2015-11-17 10:23:38 +0800

    Update createView.sql

--- SQL_MySQL/chapter-ten/createView.sql
+++ SQL_MySQL/chapter-ten/createView.sql
@@ -1,6 +1,6 @@
 # 创建视图, 不过作者在抄写代码时还是不知道"视图"的功能.
 CREATE VIEW web_designers AS 
-	SELECT mc.first_name, mc.last_name, mc.phone, mc.email
+	SELECT mc.first_name, mc.last_name,  mc.email # 没有电话号列 , mc.phone
 	FROM my_contacts mc
 	NATURAL JOIN job_desired jd;
 	-- WHERE jd.title = 'web Desi
